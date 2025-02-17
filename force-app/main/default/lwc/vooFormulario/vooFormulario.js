ecbc295c3a1a178b8f7772b98b620962d702e348
Robsu66 <robsondiaswp@gmail.com>
2025-02-17 17:26:40 -0300

    componente LWC para pesquisar aeroportos usando o código IATA

--- force-app/main/default/lwc/vooFormulario/vooFormulario.js
+++ force-app/main/default/lwc/vooFormulario/vooFormulario.js
@@ -1,21 +1,61 @@
-import { LightningElement } from 'lwc';
+import { LightningElement} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import createVoo from '@salesforce/apex/InsereVoo.createVoo';
 
-export default class AeroportoFormulario extends LightningElement {
+export default class vooFormulario extends LightningElement {
+    
+    partidaValue='';
+    chegadaValue='';
     spinner = false;
 
-    handleSubmit(){
-        this.spinner = true;
+    handlePartidaChange(event) {
+        this.partidaValue = event.detail.recordId;
+        console.log(this.partidaValue);
     }
 
-    handleSuccess(){
-        const toastEvent = new ShowToastEvent({
-            title: 'Voo Criado',
-            message:
-                'Seu voo foi criado com sucesso',
-            variant: 'success',
-        });
-        this.dispatchEvent(toastEvent);
-        this.spinner = false;
+    handleChegadaChange(event) {
+        this.chegadaValue = event.detail.recordId;
+        console.log(this.chegadaValue);
     }
+
+
+    matchingInfo = {
+        primaryField: {fieldPath: 'Name'},
+        additionalFields: [{fieldPath: 'IATA__c'}],
+    };
+
+    displayInfo = {
+        additionalFields:["IATA__c"]
+    }
+
+    handleSubmit(event) {
+            this.spinner = true;
+            event.preventDefault();
+
+            let vooInsert={
+                Partida__c: this.partidaValue,
+                Chegada__c: this.chegadaValue,
+            };
+            vooInsert = JSON.stringify(vooInsert);
+            createVoo({vooInsert:vooInsert})
+            .then(() => {
+                const toastEvent = new ShowToastEvent({
+                    title: 'Registro Criado',
+                    message: 'Seu registro foi criado com sucesso',
+                    variant: 'success',
+                });
+                this.dispatchEvent(toastEvent);
+                this.spinner = false;
+            })
+            .catch((error) => {
+                const toastEvent = new ShowToastEvent({
+                    title: 'Erro',
+                    message: error.body.message,
+                    variant: 'error',
+                });
+                this.dispatchEvent(toastEvent);
+                this.spinner = false;
+            });
+    }
+
 }
